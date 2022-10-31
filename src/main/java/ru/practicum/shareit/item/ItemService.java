83326abe6cfe9fe536f32fc9df82b7b44ba82ff4
Evgeniy <gekasan4@yandex.ru>
2022-11-01 01:08:22 +0300

    add-controller

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 public interface ItemService {
     List<Item> getAllItems(Long userId);
+
     ItemDto getItemById(Long itemId);
+
     ItemDto addItem(ItemDto itemDto, Long userId);
+
     ItemDto updateItem(ItemDto itemDto, Long userId, Long itemId);
+
     void removeItem(Long itemId);
+
     List<Item> searchItem(String text, Long userId);
 }
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers();
+
     User getUserById(Long userId);
+
     User addUser(UserDto userDto);
+
     User updateUser(UserDto userDto, Long userId);
+
     void removeUser(Long userId);
 }
