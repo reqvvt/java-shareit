47328177961d0ffa35979fb2f6a64a23724d32c9
Evgeniy <gekasan4@yandex.ru>
2022-11-23 16:19:05 +0300

    исправления после ревью

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -121,22 +121,18 @@ public class ItemServiceImpl implements ItemService {
 
     private ItemDtoInfo toItemDtoInfo(Item item, Integer ownerId) {
         Booking lastBooking = bookingRepository.findLastBooking(item.getId(), ownerId)
-                                               .orElse(null);
+                                               .orElse(null); //если здесь выкидывать исключение .ofElseThrow то не получается добавлять предметы без брони, сразу выскакивает ошибка
         Booking nextBooking = bookingRepository.findNextBooking(item.getId(), ownerId)
-                                               .orElse(null);
+                                               .orElse(null); //а так просто присваивается null и как только появляется первая бронь null заменяется на бронь, по тестам это проходит, а через исключение падает много тестов
         List<CommentDto> commentDtos = commentRepository.findAllByItemId(item.getId()).stream()
                                                         .map(CommentMapper::toCommentDto)
                                                         .collect(Collectors.toList());
         ItemDtoInfo itemDtoInfo = ItemMapper.toItemDtoInfo(item);
-        try {
+        if (lastBooking != null) {
             itemDtoInfo.setLastBooking(BookingMapper.toBookingDtoForItem(lastBooking));
-        } catch (NullPointerException e) {
-            e.getMessage();
         }
-        try {
+        if (nextBooking != null) {
             itemDtoInfo.setNextBooking(BookingMapper.toBookingDtoForItem(nextBooking));
-        } catch (NullPointerException e) {
-            e.getMessage();
         }
         itemDtoInfo.setComments(commentDtos);
         return itemDtoInfo;
